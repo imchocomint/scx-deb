77a8f6010b50facc0ee03a64b075bd4d532347f3
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2026-03-29 10:55:18 +0700

    First commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+wget https://archlinux.org/packages/extra/x86_64/scx-scheds/download/ --content-disposition
+tar -I zstd -xf scx-scheds-*.pkg.tar.zst
+rm .*
+mv  ./usr ./scx-scheds 
+rm *.tar.zst
+dpkg-deb --build scx-scheds
