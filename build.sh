bafb44169d78df57ce872d848d95b254b5c4f20f
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2026-07-26 15:37:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 wget https://archlinux.org/packages/extra/x86_64/scx-scheds/download/ --content-disposition
+wget https://archlinux.org/packages/extra/x86_64/scx-tools/download/ --content-disposition
 tar -I zstd -xf scx-scheds-*.pkg.tar.zst
+tar -I zstd -xf scx-tools-*.pkg.tar.zst
 rm .*
 mv  ./usr ./scx-scheds 
+mv  ./usr ./scx-tools
 rm *.tar.zst
 dpkg-deb --build scx-scheds
